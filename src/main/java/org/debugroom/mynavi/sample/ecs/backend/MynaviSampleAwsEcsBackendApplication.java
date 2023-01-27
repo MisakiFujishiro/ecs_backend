db2ab0d2d9e3b35ef8bfa3a8aaa75c25bd92d141
misaki_fujishiro <Misaki.Fujishiro@nttdata.com>
2023-01-27 15:05:14 +0900

    test

---
+++ src/main/java/org/debugroom/mynavi/sample/ecs/backend/MynaviSampleAwsEcsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.debugroom.mynavi.sample.ecs.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynaviSampleAwsEcsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynaviSampleAwsEcsBackendApplication.class, args);
+	}
+
+}
